e9006ef11a7297e8f2487777b71d70a7cdfed46e
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2026-01-16 23:23:10 +0530

    done some more

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php 
-$age = 15;
+$age = 65;
 
-if ($age >= 18) {
+if ($age >= 18 and $age < 60) {
     echo "you may enter";    # code...
+}
+elseif($age >= 60){
+    echo "you r too old... for this";
 }
 else {
     echo "you are not allowed...";
