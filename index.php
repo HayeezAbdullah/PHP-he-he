56d7f1563df3aa16ccc62798240dbf7166a8fe55
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2026-01-18 03:34:38 +0530

    done array

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php 
 
-//arrays
+//associative array
+$capitals = array("USA"=>"Washington DC",
+                 "Japan"=>"Kyoto",
+                 "South Korea"=>"Seoul",
+                 "India"=>"New Delhi");
 
-$foods = array("Pizza", "Burger", "Hotdog");
-$counter = count($foods);
-$a = 0;
+$capitals["USA"] = "Las Vegas";
+$capitals["China"] = "Beijing";
+// array_pop($capitals);
+// array_shift($capitals);
+$keys = array_keys($capitals);
 
-while ($a < $counter) {
-    # code...
-    echo "food: {$foods[$a]} <br>";
-    $a++;
+foreach($keys as $key){
+    echo "{$key} <br> ";
 }
+
+
 
  
 ?>
