629c22f87e9fb120f14eb5acee90ae9c67974e5d
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2026-01-18 03:53:50 +0530

    isset() and empty()

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>Username:</label>
+        <input type="text" name="username">
+        <br>
+        <br>
+        <label>Password:</label>
+        <input type="password" name="password">
+<br><br>
+        <input type="submit" value="Log In" name="login">
+        <br>
+
+    </form>
+</body>
+</html>
+
 <?php 
 
-$username = "hbjhb";
+        // foreach($_POST as $key => $value){
+        //     echo "{$key} = {$value} <br>";
+        // }
 
-if (isset($username)) {
-    echo "this var is set ";
-} else {
-    echo "this var is not set";
-}
+        if(isset($_POST["login"])){
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+
+            if (empty($username)) {
+                echo "user name is missing";
+            } elseif (empty($password)) {
+             echo "password is missing";
+            } 
+            else {
+                echo "hello, {$username}";
+            }
+        }
  
 ?>
