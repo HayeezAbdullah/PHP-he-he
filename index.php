73994bbeda717128fe47c7c08ffa975cb0809221
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2026-01-18 05:46:39 +0530

    happy_birthday(bro)

--- index.php
+++ index.php
@@ -1,35 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <input type="checkbox" name="pizza" value="Pizza">
-        Pizza <br>
-
-         <input type="checkbox" name="hamburger" value="Hamburger">
-        Hamburger <br>
-
-         <input type="checkbox" name="hotdog" value="Hotdog">
-        Hotdog <br>
-
-        <input type="submit" name="submit"  value="submit">
-    </form>
-</body>
-</html>
-
-
 <?php 
 
-if(isset($_POST["submit"])){
-
-
-    if (isset($_POST["pizza"])) {
-        echo "u selected pizza";
-    }
+function happy_birthday($name){
+    echo "happy birthday  to {$name}!";
 }
+
+happy_birthday("bro");
+
 
 ?>
