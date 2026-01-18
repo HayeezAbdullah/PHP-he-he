8692953a45b5e5844f2e95a7d435a8531700a259
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2026-01-18 06:14:13 +0530

    string functionssssss

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-</body>
-</html>
-
-
 <?php 
 
-function hypotenuse($a, $b){
-    $c = sqrt($a ** 2 + $b ** 2);
-    return $c;
-}
+$username = "HAYEEZABDULLAH";
+$username = strtolower($username);
 
-echo hypotenuse(3, 4);
+echo $username;
 
 ?>
