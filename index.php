96a35ac073c2961c5734d5b9a68e571e68bcfafd
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2026-01-18 06:45:07 +0530

    sanitizing

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        username: <br>
+    <input type="text" name="username"> <br>
+    <input type="submit" name="login" value="login"> <br>
+    </form>
+</body>
+</html>
+
 <?php 
 
-$username = "HAYEEZABDULLAH";
-$username = strtolower($username);
-
-echo $username;
+if (isset($_POST["login"])) {
+    
+    $username = $_POST["username"];
+    echo "Hello {$username}!";
+}
 
 ?>
