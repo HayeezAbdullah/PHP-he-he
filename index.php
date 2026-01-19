3a78cde8f3a3cb1ef2de478d32b92dcf41e3be10
Hayeez  Abdulllah <hayeezabdullah2k5@gmail.com>
2026-01-20 04:13:21 +0530

    data insert DONEE

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 include("database.php");
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Hello <br>
-</body>
-</html>
+$username = "batman";
+$password = "batmanisfr";
+
+$sql = "INSERT INTO user (user, password) VALUES ('$username', '$password')";
+
+try {
+    mysqli_query($conn, $sql);
+    echo "User is now registered";
+} catch (mysqli_sql_exception) {
+    echo "Could not register user";    
+}
+?>
